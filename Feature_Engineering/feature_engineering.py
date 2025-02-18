7206b24f00c89ee338c62484f4c96c4361cfd746
Felipe Marcelo <s.fmpds@gmail.com>
2025-02-18 01:13:45 -0300

    feat: create count in a period logic

--- Feature_Engineering/feature_engineering.py
+++ Feature_Engineering/feature_engineering.py
@@ -1,10 +1,13 @@
 import re
 from turtle import right
+from unittest import result
 import pandas as pd
 import numpy as np
 import glob
 
 from scipy.stats import linregress
+from sqlalchemy import column
+from datetime import timedelta
 from Scripts.functions_to_help import *
 
 def create_attributes_count_mean(df, user_type, column_name, type_column, parte_treino):
@@ -145,20 +148,52 @@ def create_count_days_by_page(df, user_type, parte_treino):
 
     table.to_parquet('./Dados/files/treino/Atributos/table_count_days_by_page_treino_{0}_{1}.parquet'.format(parte_treino, str(user_type).replace(r"-", "")))
     
+
+def create_flag_was_modified():
+    
+    BASE_PATH_TABLE_COUNT_USERID = "./Dados/files/treino/Atributos/table_count_userId_*.parquet"
+    BASE_PATH_ITENS = "./Dados/itens/itens/*.csv"
     
     
+    df_tables_count_id = {}
 
-
-def calcular_inclinacao(grupo, date_column, count_column):
-    # Converter dias para números (dias desde o primeiro dia)
-    dias = (grupo[date_column] - grupo[date_column].min()).dt.days
-    quantidades = grupo[count_column]
+    for full_path in glob.glob(BASE_PATH_TABLE_COUNT_USERID):
+        df = pd.read_parquet(full_path)
+        
+        match = re.search(r'parte(\d+)', full_path)
+        df_tables_count_id[match.group(0)] = df
     
-    # Calcular a regressão linear
-    slope, _, _, _, _ = linregress(dias, quantidades)
-    return slope
+    df_itens = {}
+    
+    for full_path in glob.glob(BASE_PATH_ITENS):
+        df = pd.read_csv(full_path)
+        
+        match = re.search(r'parte(\d+)', full_path)
+        df_itens[match.group(0)] = df
+    
+    
+    list_itens_parts = list(df_itens.keys())
 
-def create_rate_by_day(df, user_type, parte_treino):
+    list_tables_parts = list(df_tables_count_id.keys())
+    
+    
+    
+    for table_part in list_tables_parts:
+        list_df_left_join = []
+        for itens_part in list_itens_parts:
+            df_itens[itens_part]['is_modified'] = np.where(df_itens[itens_part]['issued'] == df_itens[itens_part]['modified'], 0, 1)
+            list_df_left_join.append(df_tables_count_id[table_part].merge(df_itens[itens_part][['page', 'is_modified']], left_on = 'history', right_on = 'page', how = 'left'))
+        
+        df_left_join = pd.concat(list_df_left_join, ignore_index=True)
+        df_left_join_grouped = df_left_join.groupby('history', as_index=False).first()
+    
+    
+        df_left_join_grouped[['history', 'is_modified']].to_parquet('./Dados/files/treino/Atributos/table_flag_is_modified_by_page_treino_{0}_Nonlogged.parquet'.format(table_part))
+
+
+def create_count_in_period(df, user_type, table_part):
+    
+    BASE_PATH_ITENS = "./Dados/itens/itens/*.csv"
     
     df_filtrado = df[df['userType'] == user_type]
     
@@ -171,16 +206,121 @@ def create_rate_by_day(df, user_type, parte_treino):
     df_explode = df_filtrado[['history', 'dataHistory']].apply(pd.Series.explode)
 
     df_explode['dataHistory'] = pd.to_datetime(df_explode['dataHistory'], format = "%d/%m/%Y %H:%M")
+    
+    df_itens = {}
+    
+    for full_path in glob.glob(BASE_PATH_ITENS):
+        df = pd.read_csv(full_path)
+        
+        df['issued'] = pd.to_datetime(df['issued'])
+        
+        match = re.search(r'parte(\d+)', full_path)
+        df_itens[match.group(0)] = df
+    
+    
+    list_itens_parts = list(df_itens.keys())
+    list_df_left_join = list()
+    
+    for itens_part in list_itens_parts:
+        
+        
+        list_df_left_join.append(df_explode.merge(df_itens[itens_part][['page', 'issued']], left_on = 'history', right_on = 'page', how = 'left'))
+    
+    
+    
+    df_left_join = pd.concat(list_df_left_join, ignore_index=True)
+    
+    
+    df_filtered = df_left_join[(df_left_join['dataHistory'] >= df_left_join['issued']) &
+                                (df_left_join['dataHistory'] <= df_left_join['issued'] + timedelta(days=1))]
+    
+    
+    table = df_filtered.groupby('history').size().reset_index(name='count_in_first_day')
+    table.to_parquet('./Dados/files/treino/Atributos/table_count_page_users_in_first_day_treino_{0}_Nonlogged.parquet'.format(table_part))
 
-    df_explode['dataHistory_day'] = df_explode['dataHistory'].dt.date
-    
-    
-    table_page_by_day = df_explode.groupby(['history', 'dataHistory_day']).size().reset_index(name='qnt_dias')
-    
-    table_page_by_day.sort_values(by = ['history', 'dataHistory_day'])
-    
-    table_crescimento = table_page_by_day.groupby('history').apply(calcular_inclinacao).reset_index()
-    
-    table_crescimento.columns = ['history', 'taxa_crescimento_users']
-    
-    return [table_crescimento, table_page_by_day]
+import os
+import pandas as pd
+from collections import defaultdict
+
+def merge_files_by_same_part(folder_path, key_column, output_folder):
+
+    # Dicionário para agrupar arquivos por número de parte
+    part_files = defaultdict(list)
+
+    # Percorre todos os arquivos na pasta
+    for filename in os.listdir(folder_path):
+        # Verifica se o arquivo contém "parteX" onde X é de 1 a 5
+        for i in range(1, 6):
+            if f"parte{i}" in filename:
+                # Adiciona o arquivo ao grupo correspondente
+                part_files[i].append(filename)
+                break
+
+    # Verifica se há arquivos agrupados
+    if not part_files:
+        print("Nenhum arquivo com 'parteX' encontrado.")
+        return
+
+    # Cria a pasta de saída se não existir
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Processa cada grupo de arquivos com a mesma parte
+    for part_number, files in part_files.items():
+        # Lista para armazenar os DataFrames do grupo
+        dfs = []
+
+        # Lê todos os arquivos do grupo
+        for filename in files:
+            file_path = os.path.join(folder_path, filename)
+            try:
+                # Lê o arquivo (supondo que seja CSV)
+                df = pd.read_parquet(file_path)
+                print(filename)
+                padrao = r"table_count_(.*?)_treino_parte(.*?)_Nonlogged.parquet"
+
+                # Procurar o padrão na string
+                column_count_name = re.search(padrao, filename)
+                
+                try:
+                    column_count_name = column_count_name.group(1)
+                    
+                    df.rename(columns={'total': column_count_name}, inplace = True)
+                    dfs.append(df)
+                except:
+                    
+                    dfs.append(df)
+            except Exception as e:
+                print(f"Erro ao ler o arquivo {filename}: {e}")
+
+        # Faz o merge dos DataFrames do grupo pela chave comum
+        if dfs:
+            merged_df = dfs[0]
+            for df in dfs[1:]:
+                
+                
+                
+                merged_df = pd.merge(merged_df, df, on=key_column, how='inner')
+
+            # Salva o arquivo final do grupo
+            output_file = os.path.join(output_folder, f"treino_atributos_{part_number}_Nonlogged.parquet")
+            merged_df.to_parquet(output_file, index=False)
+            print(f"Arquivo final salvo em: {output_file}")
+        else:
+            print(f"Nenhum DataFrame válido para parte {part_number}.")
+
+# Exemplo de uso
+folder_path = './Dados/files/treino/Atributos/'
+key_column = 'history'  # Substitua pelo nome da coluna chave
+output_folder = './Dados/files/treino/Treino/'
+
+merge_files_by_same_part(folder_path, key_column, output_folder)
+
+text = "table_count_numberOfClicksHistory_by_page_treino_parte1_Nonlogged.parquet"
+padrao = r"table_count_(.*?)_treino_parte1_Nonlogged.parquet"
+
+# Procurar o padrão na string
+resultado = re.search(padrao, text)
+resultado.group(1)
+
+
+df_treino_atributos = pd.read_parquet('./Dados/files/treino/Treino/treino_atributos_1_Nonlogged.parquet')
